5fbe5b4ad74587f2e39fd31f6659ee6cc7d65da4
nextlvlpro <80281253+nextlvlpro@users.noreply.github.com>
2024-01-15 22:37:24 +0530

    Add files via upload

---
+++ convert.js
@@ -0,0 +1,33 @@
+let input = 'the big cat jumped over dog'
+
+function genrateOutput (input) {
+    
+    //Convert String to array
+    let data = [...input].join("").split(' ')
+    
+    // Empty string where results will be stored.
+    let output = ''
+
+    //starting and ending points for loop
+    let l = 0
+    let r = data.length-1
+    let m = Math.floor(data.length/2)
+
+    
+    // This will loop in a way that first and last elemnt of string will be stored and it will move to the next element
+    while (l<m) {
+        output += data[r].split('').reverse().join('')+ " "
+        r--
+        output += data[l] .split('').reverse().join('')+ " "
+        l++
+    }  
+
+    // Output is printed on screen on Web Browser. Remove this line if you want to print results directly on terminal in node js.
+    document.querySelector(".output").innerText +=` "${output}"`
+
+    console.log(output);
+    
+}
+
+genrateOutput(input)
+
